b07320feed947c63cad446405c4155a837a30274
Dan <dan.sasaki12@gmail.com>
2018-05-19 11:23:24 -0600

    update

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,22 @@
+import { connect } from 'http2';
+
 // Pull in required dependencies
 var mysql = require('mysql');
 
 // Create the MySQL connection object
 var connection;
-
+if (process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 	connection = mysql.createConnection({
 		port: 3306,
 		host: 'localhost',
 		user: 'root',
 		password: '',
 		database: 'burgers_db'
+	
 	});
-
+};
 // Make the connection to MySQL
 connection.connect(function(err) {
   if (err) {
