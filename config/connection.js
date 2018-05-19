b73f15ecb282b81694a3437b4e5e7d5505de7765
Dan <dan.sasaki12@gmail.com>
2018-05-19 12:05:47 -0600

    update

--- config/connection.js
+++ config/connection.js
@@ -7,13 +7,14 @@ var mysql = require('mysql');
 var connection;
 if (process.env.JAWSDB_URL){
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
+
 } else {
 	connection = mysql.createConnection({
 		port: 3306,
-		host: 'localhost',
-		user: 'root',
-		password: '',
-		database: 'burgers_db'
+		host: 'umabrisfx8afs3ja.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		user: 'bcbhftqxhf2wqk2q',
+		password: 'grum2nfmr39awccs',
+		database: 'ex1fri1zcwimcsoc'
 	
 	});
 };
